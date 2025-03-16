6429703f7e04c927f84178af50bd8548e88d5ce1
Hampus Avekvist <ha@leissner.se>
2025-03-16 16:36:50 +0100

    docs(whitepaper): Finish halftime presentation

--- docs/whitepaper/entrypoint.sh
+++ docs/whitepaper/entrypoint.sh
@@ -9,6 +9,10 @@ pdflatex --shell-escape main
 biber main
 pdflatex --shell-escape main
 
+pdflatex --shell-escape halftime-presentation
+biber halftime-presentation
+pdflatex --shell-escape halftime-presentation
+
 pdflatex --shell-escape proposal
 biber proposal
 pdflatex --shell-escape proposal
