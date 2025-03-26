f8dcb1e2e14b92cc2576f19fc3ffa2f483ea3baf
Hampus Avekvist <ha@leissner.se>
2025-03-26 13:55:45 +0100

    feat(prototype): Run SIM bank multithreaded and handle a card once

--- prototype/src/simbank.cc
+++ prototype/src/simbank.cc
@@ -10,13 +10,21 @@
 
 #include "atp.hh"
 #include "data.hh"
-#include "flag-set-cpp/flag_set.hpp"
 
 using namespace pcsc_cpp;
 using namespace std::literals;
 
-struct InternalMessage {
+struct Exit {};
+
+struct ReaderStateChange {
     std::string readerName;
+};
+
+using InternalMessage = std::variant<ReaderStateChange, Exit>;
+
+template <class... Ts>
+struct Overloads : Ts... {
+    using Ts::operator()...;
 };
 
 using Queue = std::queue<InternalMessage>;
@@ -27,39 +35,66 @@ auto handleReaders(Queue& queue, Cards& cards, std::stop_token token) {
         return;
     }
 
-    auto readers       = listReaders();
-    auto readerChanged = [](Reader reader) { return static_cast<bool>(reader.status & Reader::Status::CHANGED); };
+    auto readers             = listReaders();
+    auto readerExistsInCards = [&cards](Reader reader) { return cards.find(reader.name) != cards.end(); };
 
-    for (auto reader : readers) {
-        std::println("{}: {}", reader.name, reader.statusString());
-    }
-
-    for (auto reader : readers | std::views::filter(readerChanged)) {
-        if (reader.isCardInserted()) {
-            std::println("Card inserted into reader {}", reader.name);
-            cards[reader.name] = reader.connectToCard();
-            queue.emplace(reader.name);
-        } else {
+    for (auto reader : readers | std::views::filter(readerExistsInCards)) {
+        if (!reader.isCardInserted()) {
             std::println("Removing card from reader {}", reader.name);
             cards.erase(reader.name);
+        }
+    }
+
+    for (auto reader :
+         readers | std::views::filter([&readerExistsInCards](auto reader) { return !readerExistsInCards(reader); })) {
+        if (reader.isCardInserted()) {
+            std::println("Card inserted into reader {}", reader.name);
+            try {
+                cards[reader.name] = reader.connectToCard();
+                queue.emplace(ReaderStateChange{reader.name});
+            } catch (...) {
+                std::println(stderr, "Could not connect to card in reader {}", reader.name);
+            }
         }
     }
 
     std::this_thread::yield();
 }
 
-auto handleReceive(TcpServer& server, std::stop_token token) {
+auto handleReceive(TcpServer& server, Queue& queue, std::stop_token token) {
     if (token.stop_requested()) {
         return;
     }
 
     auto message = server.read<Atp>();
-    std::println("Receiving {}", message);
+    if (message.length <= 0) {
+        queue.emplace(Exit{});
+        return;
+    }
+
+    std::println("Received {}", message);
 
     std::this_thread::yield();
 }
 
-auto handleQueue(TcpServer& server, Queue& queue, Cards& cards) {
+auto handleReaderStateChange(TcpServer& server, Cards& cards, ReaderStateChange state) {
+    auto atp = Atp{
+        1,
+        Type::Atr,
+        GenericData{cards[state.readerName]->atr()},
+    };
+
+    std::println("{}", atp);
+    server.write<GenericData>(atp);
+}
+
+auto handleExit(std::stop_source& stop_source) {
+    if (stop_source.stop_possible()) {
+        stop_source.request_stop();
+    }
+}
+
+auto handleQueue(TcpServer& server, Queue& queue, Cards& cards, std::stop_source& stop_source) {
     if (queue.empty()) {
         std::this_thread::yield();
         return;
@@ -68,14 +103,9 @@ auto handleQueue(TcpServer& server, Queue& queue, Cards& cards) {
     auto element = queue.front();
     queue.pop();
 
-    auto atp = Atp{
-        1,
-        Type::Atr,
-        GenericData{cards[element.readerName]->atr()},
-    };
-
-    std::println("{}", atp);
-    server.write<GenericData>(atp);
+    std::visit(Overloads{[&server, &cards](ReaderStateChange state) { handleReaderStateChange(server, cards, state); },
+                         [&stop_source](Exit) { handleExit(stop_source); }},
+               element);
 }
 
 int main(int, char*[]) {
@@ -91,15 +121,15 @@ int main(int, char*[]) {
                                       },
                                       stop_source.get_token()};
 
-    auto receiveHandler = std::jthread{[&server](std::stop_token token) {
+    auto receiveHandler = std::jthread{[&queue, &server](std::stop_token token) {
                                            while (!token.stop_requested()) {
-                                               handleReceive(server, token);
+                                               handleReceive(server, queue, token);
                                            }
                                        },
                                        stop_source.get_token()};
 
     while (!stop_source.stop_requested()) {
-        handleQueue(server, queue, cards);
+        handleQueue(server, queue, cards, stop_source);
     }
 
     // Make-shift "exit" signal to peer
