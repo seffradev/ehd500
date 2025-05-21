ad1696ac670f3fa596a255b12cc89106c7e819dc
Hampus Avekvist <ha@leissner.se>
2025-05-21 13:10:47 +0200

    chore: Clean up unused material

--- docs/whitepaper/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-set -xe
-
-pdflatex --shell-escape halftime-presentation
-biber halftime-presentation
-pdflatex --shell-escape halftime-presentation
-
-pdflatex --shell-escape proposal
-biber proposal
-pdflatex --shell-escape proposal
